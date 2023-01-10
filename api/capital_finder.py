ea3e317da0a24a99e1c1eb8eb500af9c5463597e
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-09 16:59:36 -0800

    added capital_finder script

---
+++ api/capital_finder.py
@@ -0,0 +1,38 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        print(f"the dic is {dic}")
+
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
+            data = r.json()
+            capital = data[0]['capital'][0]
+            message = f"The capital of {dic['country']} is {capital}"
+
+        if "capital" in dic:
+            url = "https://restcountries.com/v3.1/capital/"
+            r = requests.get(url + dic["capital"])
+            data = r.json()
+            country = data[0]['name']['common']
+            message = f"{dic['capital']} is the capital of {country}"
+
+
+        else:
+            message = "Type a country into the query"
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
